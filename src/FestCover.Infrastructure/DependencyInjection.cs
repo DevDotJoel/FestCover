a533de0f3310b93c057bfe9a442a6608b08e0212
Joel Ferreira <j141996@hotmail.com>
2025-02-03 16:01:33 +0000

    aed

--- src/FestCover.Infrastructure/DependencyInjection.cs
+++ src/FestCover.Infrastructure/DependencyInjection.cs
@@ -20,7 +20,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using FestCover.Infrastructure.Common.Services;
 using FestCover.Infrastructure.Content;
 using FestCover.Infrastructure.Payment;
 
